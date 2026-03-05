288738ec398fdd7db37b8f67c31fd93d8ca08913
abhishek <ak3221110@gmail>
2026-03-05 22:19:26 +0530

    P_data type

---
+++ datatype_input.py
@@ -0,0 +1,27 @@
+#primtive data type in python
+#integer
+
+n=45
+n1=-45
+n2=0
+print(n,type(n))
+print(n1,type(n1))
+print(n2,type(n2))
+
+f=5.0
+f1=5.
+f2=0.00
+print(f,type(f))
+print(f1,type(f1))
+print(f2,type(f2))
+
+
+#Bolean
+x=True
+y=False
+x1="True"
+print(x,type(x))
+print(y,type(y))
+print(x1,type(x1))
+
+#String
